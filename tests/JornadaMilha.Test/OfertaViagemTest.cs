f2e71f815b562493d1dd73b52446f55d5261f369
ThiagoMelo22 <thiagomelo329@hotmail.com>
2026-09-02 19:54:05 -0300

    adiciona teste com rota de oferta nula

--- tests/JornadaMilha.Test/OfertaViagemTest.cs
+++ tests/JornadaMilha.Test/OfertaViagemTest.cs
@@ -7,15 +7,35 @@ namespace JornadaMilha.Test
         [Fact]
         public void TestandoOfertaValida()
         {
+            //cenário - arrange
             Rota rota = new Rota("OrigemTeste", "DestinoTeste");
             Periodo periodo = new Periodo(new DateTime(2024,2,1), new DateTime(2024,2,5));
             double preco = 100.00;
 
             var validacao = true;
 
+            //ação - action
             OfertaViagem oferta = new(rota, periodo, preco);
             
+            //validação - assert
             Assert.Equal(validacao, oferta.EhValido);
+        }
+
+        [Fact]
+        public void TestandoOfertaComRotaNula()
+        {
+            //cenário - arrange
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.00;
+
+            //ação - action
+            OfertaViagem oferta = new(rota, periodo, preco);
+            var mensagem = "A oferta de viagem não possui rota ou período válidos.";
+
+            //validação - assert
+            Assert.Contains(mensagem, oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
         }
     }
 }
